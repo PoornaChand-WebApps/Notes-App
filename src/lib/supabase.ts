f0b206ad2339592a2369d19051f74be70be68107
Poorna Chand <poornachandy2021@gmail.com>
2025-05-14 05:03:46 -0500

    updated env ariables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://sfqwuzosrbrdcqcrkaok.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmcXd1em9zcmJyZGNxY3JrYW9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4NDE4MTIsImV4cCI6MjA2MjQxNzgxMn0.3iJY_OWHBiXGcwRAxBMcM5VrVaVUW7BWB1MTVI3g6WM';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
